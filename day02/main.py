c7a291fc2111493d77110b40d7b29ee1ffd2b652
George <g.nikolaropoulos@gmail.com>
2022-12-03 20:56:02 +0200

    adding day 2

--- day02/main.py
+++ day02/main.py
@@ -7,22 +7,24 @@ part2_combined_scores = [[3, 4, 8],
                          [1, 5, 9],
                          [2, 6, 7]]
 
-letterToIndexMapping = {"A": 0, "B": 1, "C": 2, "X": 0, "Y": 1, "Z": 2}
+lettersToIndeces = {"A": 0, "B": 1, "C": 2, "X": 0, "Y": 1, "Z": 2}
 
 def solve_part_1(input):
-    total_score = 0
+    score = 0
     for line in input:
-        choices = line.split(" ")
-        total_score += part1_combined_scores[letterToIndexMapping.get(choices[0])][letterToIndexMapping.get(choices[1])]
-    return total_score
+        player1 = line.split(" ")[0]
+        player2 = line.split(" ")[1]
+        score += part1_combined_scores[lettersToIndeces.get(player1)][lettersToIndeces.get(player2)]
+    return score
 
 
 def solve_part_2(input):
-    total_score = 0
+    score = 0
     for line in input:
-        choices = line.split(" ")
-        total_score += part2_combined_scores[letterToIndexMapping.get(choices[0])][letterToIndexMapping.get(choices[1])]
-    return total_score
+        player1 = line.split(" ")[0]
+        player2 = line.split(" ")[1]
+        score += part2_combined_scores[lettersToIndeces.get(player1)][lettersToIndeces.get(player2)]
+    return score
 
 
 def get_puzzle_input():
